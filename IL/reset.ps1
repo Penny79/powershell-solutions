1ceb8481d3e6a1faa0388ab67651d84df99c7adc
Penny79 <Ralf.Hoeppner@gmail.com>
2017-06-22 20:45:13 +0200

    cleanup

--- IL/reset.ps1
+++ IL/reset.ps1
@@ -1,3 +1,3 @@
 Remove-Item -Path "$PSScriptRoot\output\*" 
 Remove-Item -Path "$PSScriptRoot\input\*" 
-Copy-Item -Path "$PSScriptRoot\201702_vom_20170130_Fahrplan_2017_01 (2).xls" "$PSScriptRoot\input\"
+Copy-Item -Path "$PSScriptRoot\testfile.xls" "$PSScriptRoot\input\"
